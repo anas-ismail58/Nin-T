f6015252d2b516011c0f25a55eb0408bd0744eb0
anas ismail <anasismail58@gmail.com>
2023-06-02 17:05:58 +0300

    side menu collapse test

--- script.js
+++ script.js
@@ -2,35 +2,35 @@
 const pageWrapper = document.createElement('div');
 pageWrapper.classList.add('form-nintex__wrapper');
 
-
+ 
 
 const sideMenu = document.createElement('div');
 sideMenu.classList.add('side-menu');
 // Add an ID to the side menu for easier DOM selection later
 sideMenu.setAttribute('id', 'side-menu');
 
-
+ 
 
 const formWrapper = document.createElement('div');
 formWrapper.classList.add("form-wrapper");
 const forms = document.querySelector('.theme-entry .runtime-form');
 formWrapper.appendChild(forms);
 
-
+ 
 
 // Create the main category list
 const mainList = document.createElement('ul');
 
-
+ 
 
 // Create the first main category and its subcategories
 const mainItem1 = document.createElement('li');
 const mainLink1 = document.createElement('a');
 mainLink1.textContent = 'Main Category 1';
-// Add an event listener to the main category link to toggle the 'collapsed' class on the side menu
-mainLink1.addEventListener('click', function () {
-    const sideMenu = document.getElementById('side-menu');
-    sideMenu.classList.toggle('collapsed');
+// Add an event listener to the main category link to toggle the 'collapsed' class on the page wrapper
+mainLink1.addEventListener('click', function() {
+  const pageWrapper = document.querySelector('.form-nintex__wrapper');
+  pageWrapper.classList.toggle('collapsed');
 });
 const subList1 = document.createElement('ul');
 for (let i = 1; i <= 3; i++) {
@@ -43,15 +43,15 @@ for (let i = 1; i <= 3; i++) {
 mainItem1.appendChild(mainLink1);
 mainItem1.appendChild(subList1);
 
-
+ 
 
 // Create the second main category and its subcategories
 const mainItem2 = document.createElement('li');
 const mainLink2 = document.createElement('a');
 mainLink2.textContent = 'Main Category 2';
-mainLink2.addEventListener('click', function () {
-    const sideMenu = document.getElementById('side-menu');
-    sideMenu.classList.toggle('collapsed');
+mainLink2.addEventListener('click', function() {
+  const pageWrapper = document.querySelector('.form-nintex__wrapper');
+  pageWrapper.classList.toggle('collapsed');
 });
 const subList2 = document.createElement('ul');
 for (let i = 1; i <= 3; i++) {
@@ -64,15 +64,15 @@ for (let i = 1; i <= 3; i++) {
 mainItem2.appendChild(mainLink2);
 mainItem2.appendChild(subList2);
 
-
+ 
 
 // Create the third main category and its subcategories
 const mainItem3 = document.createElement('li');
 const mainLink3 = document.createElement('a');
 mainLink3.textContent = 'Main Category 3';
-mainLink3.addEventListener('click', function () {
-    const sideMenu = document.getElementById('side-menu');
-    sideMenu.classList.toggle('collapsed');
+mainLink3.addEventListener('click', function() {
+  const pageWrapper = document.querySelector('.form-nintex__wrapper');
+  pageWrapper.classList.toggle('collapsed');
 });
 const subList3 = document.createElement('ul');
 for (let i = 1; i <= 3; i++) {
@@ -85,19 +85,19 @@ for (let i = 1; i <= 3; i++) {
 mainItem3.appendChild(mainLink3);
 mainItem3.appendChild(subList3);
 
-
+ 
 
 // Add the main categories to the list
 mainList.appendChild(mainItem1);
 mainList.appendChild(mainItem2);
 mainList.appendChild(mainItem3);
 
-
+ 
 
 // Add the list to the side menu container
 sideMenu.appendChild(mainList);
 
-
+ 
 
 // Add the side menu container to the page
 document.body.appendChild(pageWrapper);
