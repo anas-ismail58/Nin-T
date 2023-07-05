206ff53829d6c22ab7ea435f8e4ea71590663190
anas ismail <anasismail58@gmail.com>
2023-07-05 12:35:05 +0300

    popup change text

--- layout.js
+++ layout.js
@@ -29,7 +29,9 @@ $('[name="TitlePage2"]').closest(".row").addClass('title-page__container');
 $('[name="TitlePage3"]').closest(".row").addClass('title-page__container');
 $('[name="TitlePage4"]').closest(".row").addClass('title-page__container');
 
-$('.popup-header-text').text('البحث');
+$('.picker-search').click(function () {
+    $('.popup-header-text').text('البحث');
+})
 
 
 $('.SenderName').closest(".row").addClass('table-chat');
