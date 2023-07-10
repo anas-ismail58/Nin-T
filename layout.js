c8905c332e4f7465243e7ad1bcdc1fa86512fc92
anas ismail <anasismail58@gmail.com>
2023-07-10 11:43:10 +0300

    timeline new

--- layout.js
+++ layout.js
@@ -71,6 +71,8 @@ setTimeout(function () {
 
     for (let i = 0; i < timelineContent.length; i++) {
         $('[name="Main Page Area Item12"]').append(timelineContent[i]);
+        $('[name="Main Page Area Item15"]').append(timelineContent[i]);
+
         timelineContent[0].classList.add('firsit-timeline');
     }
 }, 500);
